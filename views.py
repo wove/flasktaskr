aeeff93bdfcce88dbfe10b6ae9880420d1673d23
Tomlin Leniston <leniston@gmail.com>
2015-04-18 13:51:23 +1000

    initial views for adding, updating, and deleting tasks, as well as a template for viewing tasks

--- views.py
+++ views.py
@@ -37,6 +37,7 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
 
+# List tasks and show new task form
 @app.route('/tasks/')
 @login_required
 def tasks():
@@ -50,3 +51,44 @@ def tasks():
                             form=AddTaskForm(request.form),
                             open_tasks=open_tasks,
                             closed_tasks=closed_tasks)
+
+# Add a new task
+@app.route('/add/', methods=['POST'])
+@login_required
+def new_task():
+    g.db = connect_db()
+    name = request.form['name']
+    date = request.form['due_date']
+    priority = request.form['priority']
+    if not name or not date or not priority:
+        flash('All fields are required. Please try again.')
+        return redirect(url_for('tasks'))
+    else:
+        g.db.execute("INSERT INTO tasks (name, due_date, priority, status) VALUES(?, ?, ?, 1)", [name, date, priority])
+        g.db.commit()
+        g.db.close()
+        flash('New task was successfully added.')
+        return redirect(url_for('tasks'))
+
+# Mark task as complete
+@app.route('/complete/<int:task_id>/')
+@login_required
+def complete(task_id):
+    g.db = connect_db()
+    g.execute("UPDATE tasks SET status=0 WHERE task_id=" + str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash('The task was marked as complete.')
+    return redirect(url_for('tasks'))
+
+# Delete a task
+@app.route('/delete/<int:task_id>/')
+@login_required
+def delete_entry(task_id):
+    g.db = connect_db()
+    g.execute("DELETE FROM tasks WHERE task_id=" + str(task_id))
+    g.db.commit()
+    g.db.close()
+    flash('The task was deleted.')
+    return redirect(url_for('tasks'))
+
