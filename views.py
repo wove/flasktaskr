24c9ba5dd33fa7fab69bff3333d33ff06ab26fd0
Tomlin Leniston <leniston@gmail.com>
2015-04-18 11:44:15 +1000

    added initial templates and css

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
 from functools import wraps
 import sqlite3
 
@@ -36,3 +36,17 @@ def login():
             return redirect(url_for('tasks'))
     if request.method == 'GET':
         return render_template('login.html')
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+    g.db = connect_db()
+    cur = g.db.execute("SELECT name, due_date, priority, task_id FROM tasks WHERE status=1")
+    open_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    cur = g.db.execute("SELECT name, due_date, priority, task_id FROM tasks WHERE status=0")
+    closed_tasks = [dict(name=row[0], due_date=row[1], priority=row[2], task_id=row[3]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('tasks.html',
+                            form=AddTaskForm(request.form),
+                            open_tasks=open_tasks,
+                            closed_tasks=closed_tasks)
